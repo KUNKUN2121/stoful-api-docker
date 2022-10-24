43e61670ec33c535a8e1b8e5747d2c5a89bd4a16
KUNKUN2121 <kunkun7532@gmail.com>
2022-10-24 19:17:13 +0900

    moreinfo

--- src/api/changePostQuantity.php
+++ src/api/changePostQuantity.php
@@ -2,24 +2,22 @@
 header('Content-Type: application/json');
 require_once "database.php";
 
+// 初期値設定
 $return["error"] = false;
+$return["moreinfo"] = false;
 $return["msg"] = "";
 
-$return["barnum"] = $_POST["barnum"];
-$return["quantity"] = $_POST["quantity"];
-
+// POSTの値を取得
 $barnum = $_POST["barnum"];
 $quantity = $_POST["quantity"];
-// if($barcode==null || $quantity==null){
-//     $return["error"] = true;
-//     $return["msg"] = "DontSet";
-//     echo json_encode($return);
-//     return;
-// }
 
-//array to return
+//確認用
+$return["barnum"] = $barnum;
+$return["quantity"] = $quantity;
 
-
+// moreinfo 0の場合
+if($_POST["moreinfo"]==0){
+    $return["moreinfo"] = false;
         try{
         // $stmt = $pdo->prepare('INSERT INTO product_contents (itemname, barnum, extension, quantity, category, price, created_at, updated_at) VALUES(:itemname, :barnum, :extension, :quantity, :category, :price, NOW(), NOW() )');
         $stmt = $pdo->prepare('UPDATE product_contents SET quantity = :quantity WHERE barnum = :barnum');
@@ -38,7 +36,48 @@ $quantity = $_POST["quantity"];
         echo 'error: ' . $e->getMessage();
         $return["msg"] =   $e->getMessage();
         }
+    }else{
+        $return["moreinfo"] = true;
+        $itemname = $_POST["itemname"];
+        $price = $_POST["price"];
+        $category = $_POST["category"];
+        $imgURL = $_POST["imgURL"];
+        $return["itemname"] = $itemname;
+        $return["price"] = $price;
+        $return["category"] = $category;
+        $return["imgURL"] = $imgURL;
+        $extension = 'jpg';
 
+        if(isset($_POST["image"])){
+            $image = file_get_contents($_POST["image"]);
+        
+            $save_path = "../img/".$barnum.".jpg" ;
+        
+            file_put_contents($save_path, $image);
+        }
+        try{
+            // $stmt = $pdo->prepare('INSERT INTO product_contents (itemname, barnum, extension, quantity, category, price, created_at, updated_at) VALUES(:itemname, :barnum, :extension, :quantity, :category, :price, NOW(), NOW() )');
+            $stmt = $pdo->prepare('UPDATE product_contents SET itemname = :itemname, price = :price, category = :category WHERE barnum = :barnum');
+    
+            // 値をセット
+            $stmt->bindValue(':barnum', $barnum);
+            $stmt->bindValue(':itemname', $itemname);
+            $stmt->bindValue(':price', $price);
+            $stmt->bindValue(':category', $category);
+        
+            // SQL実行
+            $stmt->execute();
+            echo('データベース追加しました。');
+            $return["msg"] = 'データベース追加しました。';
+            //echo $barcode;
+            //echo $content;
+        } catch (PDOException $e) {
+            // 接続できなかったらエラー表示
+            echo 'error: ' . $e->getMessage();
+            $return["msg"] =   $e->getMessage();
+            }
+
+    }
 
 // tell browser that its a json data
 echo json_encode($return);

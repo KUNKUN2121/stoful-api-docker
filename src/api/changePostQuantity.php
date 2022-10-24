1c5fc6b4fee9fafc63ae797e9dde50146f6be7a7
KUNKUN2121 <kunkun7532@gmail.com>
2022-10-24 13:04:17 +0900

    changePost変更

--- src/api/changePostQuantity.php
+++ src/api/changePostQuantity.php
@@ -1,33 +1,36 @@
 <?php
 require_once "database.php";
-
+header('Content-Type: application/json');
 
 $return["error"] = false;
 $return["msg"] = "";
 
+$return["barnum"] = $_POST["barnum"];
+$return["quantity"] = $_POST["quantity"];
 
 $barnum = $_POST["barnum"];
 $quantity = $_POST["quantity"];
 
-
-//array to return
-
         try{
+        // $stmt = $pdo->prepare('INSERT INTO product_contents (itemname, barnum, extension, quantity, category, price, created_at, updated_at) VALUES(:itemname, :barnum, :extension, :quantity, :category, :price, NOW(), NOW() )');
         $stmt = $pdo->prepare('UPDATE product_contents SET quantity = :quantity WHERE barnum = :barnum');
+
         // 値をセット
-        $stmt->bindValue(':barnum', $barcode);
+        $stmt->bindValue(':barnum', $barnum);
         $stmt->bindValue(':quantity', $quantity);
     
         // SQL実行
         $stmt->execute();
         echo('データベース追加しました。');
+        //echo $barcode;
+        //echo $content;
     } catch (PDOException $e) {
         // 接続できなかったらエラー表示
         echo 'error: ' . $e->getMessage();
         $return["msg"] =   $e->getMessage();
         }
 
-header('Content-Type: application/json');
+
 // tell browser that its a json data
 echo json_encode($return);
 //converting array to JSON string

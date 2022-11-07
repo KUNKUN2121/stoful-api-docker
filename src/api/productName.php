0f674be0121f1d28b976ba9c0625eb91e6c0bae7
KUNKUN2121 <kunkun7532@gmail.com>
2022-11-07 14:37:55 +0900

    詳細表示

--- src/api/productName.php
+++ src/api/productName.php
@@ -1,113 +1,75 @@
 <?php 
 require_once "database.php";
+
+$return["error"] = false;
+$return["msg"] = "";
+
 $barcode = $_GET['barcode'];
-if($barcode==NULL){
-    echo('productName.php?barcode=222224');
-    return;
-}
+
+if($barcode!=NULL){
+    $return["msg"] = "?barcode=XXXXXXXXXX";
+
 $img = $ServerURL.'img/';
-    // itemname 取得
-        try{
-            //$barcode = 4549131970258;
-            $stmt = $pdo->prepare('SELECT itemname FROM product_contents WHERE barnum = :barnum');
-            $stmt->bindValue(':barnum', $barcode);
-            //実行
-            $res = $stmt->execute();
 
-            // データを取得
-            if( $res ) {
-                $data = $stmt->fetch();
-                //表示処理
-                if($data[0] != null){
-                    $itemname  = $data[0];
-                    $return = 0; // 完了通知
-                }else{
-                    //登録されてない
-                    $itemname = 'error';
-                    $return = 1; // 処理終了
-                }
-            }
-        } catch (PDOException $e) {
-            // 接続できなかったらエラー表示
-            echo 'error: ' . $e->getMessage();
+try{
+    $stmt = $pdo->prepare('SELECT * FROM product_contents WHERE barnum = :barnum');
+    $stmt->bindValue(':barnum', $barcode);
+    $res = $stmt->execute();
+
+    // データを取得
+    if( $res ) {
+        $data = $stmt->fetch();
+        //値入力
+        if($data[0] != null){
+            $id= $data[0];
+            $itemname = $data[1];
+            $barcode = $data[4];
+            $quantity = $data[2];
+            $category = $data[9];
+            $price= $data[3];
+            $imgURL = $img.$barcode.'.'.$data[5];
+            $like_count = $data[6];
+            $created_at = $data[7];
+            $uploaded_at = $data[8];
+            $return = 0;
+        }else{
+            //登録されてない
+            $itemname = 'error';
+            $return = 1; // 処理終了
         }
-    //拡張子取得
-        try{
-            $stmt = $pdo->prepare('SELECT extension FROM product_contents WHERE barnum = :barnum');
-            $stmt->bindValue(':barnum', $barcode);
-            //実行
-            $res = $stmt->execute();
-
-            // データを取得
-            if( $res ) {
-                $data = $stmt->fetch();
-                //表示処理
-                if($data[0] != null){
-                    $extension = $data[0];
-                    $imgURL = $img.$barcode.'.'.$extension;
-                    
-                }else{
-                    // 拡張子がない
-                    $imgURL = 'nasi';
-                    $return = 1; // 処理終了
-                }
-            }
-        } catch (PDOException $e) {
-            // 接続できなかったらエラー表示
-            echo 'error: ' . $e->getMessage();
-        }
-
-
-        // 個数 取得
-        try{
-            //$barcode = 4549131970258;
-            $stmt = $pdo->prepare('SELECT quantity FROM product_contents WHERE barnum = :barnum');
-            $stmt->bindValue(':barnum', $barcode);
-            //実行
-            $res = $stmt->execute();
-
-            // データを取得
-            if( $res ) {
-                $data = $stmt->fetch();
-                //表示処理
-                if($data[0] !== null){
-                    $quantity  = $data[0];
-                    $return = 0; // 完了通知
-                }else{
-                    //登録されてない
-                    $itemname = 'error';
-                    $return = 1; // 処理終了
-                }
-            }
-        } catch (PDOException $e) {
-            // 接続できなかったらエラー表示
-            echo 'error: ' . $e->getMessage();
-        }
-
-
+    }
+} catch (PDOException $e) {
+        // なんらかのエラーが発生した。
+    echo 'error: ' . $e->getMessage();
+}
 
     // json 変換処理
     if($return == 0){
         $person = [
+        'id' => $id,
         'itemname' => $itemname,
         'barcode' => $barcode,
         'quantity' => $quantity,
-        'category' => 'todocategory',
-        'price' => 1000,
+        'category' => $category,
+        'price' => $price,
         'imgURL' => $imgURL,
+        'like_count' => $like_count,
+        'created_at' => $created_at,
+        'uploaded_at' => $uploaded_at,
+
         ];
+    }
+    else{
+        $person = [
+            'id' => $null,
+            // 'msg' => ;
+        ];
+        header('HTTP/1.1 400 Bad Request');
+        header('Content-Type: application/json');
+    }
+}
         header('Content-Type: application/json');
         echo json_encode($person);
         /////////////デバッグ用日本語で表示
         // echo json_encode($person, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-
-    // 登録情報なし
-    }elseif ($return == 1){
-        header('HTTP/1.1 400 Bad Request');
-        header('Content-Type: application/json');
-        echo('存在しません。');
-    // $returnが定義されていない サーバエラー
-    }else{
-        header('HTTP/1.1 500 Internal Server Error');
-    }
 ?>

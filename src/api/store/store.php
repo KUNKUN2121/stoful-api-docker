df1e051a9c24a977ee8287c9f2fa7d1bb6724d85
KUNKUN2121 <kunkun7532@gmail.com>
2022-11-23 18:09:48 +0900

    fix: 日本時間

--- src/api/store/store.php
+++ src/api/store/store.php
@@ -1,18 +1,18 @@
 <?php
-// token認証
+header('Content-Type: application/json');
 $token = 'gzeK6Q1MWhSKzOMV';
 if($_POST["token"] != $token){
     header("HTTP/1.0 401 Unauthorized");
     return;
 }
-header('Content-Type: application/json');
-require_once "../database.php";
 
+require_once "../database.php";
 $return["error"] = false;
 $return["msg"] = "";
-
 $barnum = $_POST["barnum"];
 
+$date = new DateTime();
+$date = $date->format('Y-m-d H:i:s');
 
 try {
     //商品があるか確認
@@ -24,13 +24,16 @@ try {
         // 在庫あり //
 
         // 個数変更処理
-        $stmt = $pdo->prepare('UPDATE product_contents SET quantity = quantity + -1 , updated_at = NOW() WHERE barnum = :barnum');
+        $stmt = $pdo->prepare('UPDATE product_contents SET quantity = quantity + -1 , updated_at = :updated_at WHERE barnum = :barnum');
         $stmt->bindValue(':barnum', $barnum);
+        $stmt->bindValue(':updated_at', $date, PDO::PARAM_STR);
         $stmt->execute();
 
         // 売上票記入用
-        $stmt = $pdo->prepare('INSERT INTO earnings (barnum, created) VALUES(:barnum, NOW())');
+        $stmt = $pdo->prepare('INSERT INTO earnings (barnum, created) VALUES(:barnum, :created)');
+        // $stmt->bindValue(':date', $created_at->format('Y-m-d H:i:s'), PDO::PARAM_STR);
         $stmt->bindValue(':barnum', $barnum);
+        $stmt->bindValue(':created', $date, PDO::PARAM_STR);
         $stmt->execute();
         $return["msg"] = 'Done itemname : '. $check_data[1]; 
 

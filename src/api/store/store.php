7bdff1b5a41eb2798f0b0183495e2b3adc9c8a6e
KUNKUN2121 <kunkun7532@gmail.com>
2022-11-25 08:43:40 +0900

    fix : 在庫なし表示

--- src/api/store/store.php
+++ src/api/store/store.php
@@ -20,28 +20,38 @@ try {
     $checkpro->bindValue(':barnum', $barnum);
     $res = $checkpro->execute();
     $check_data = $checkpro->fetch(PDO::FETCH_BOTH);
-    if($check_data[2]>0){
-        // 在庫あり //
+    if($check_data != false){
+        //データベースあり
+        if($check_data[2]>0){
+            // 在庫あり //
+    
+            // 個数変更処理
+            $stmt = $pdo->prepare('UPDATE product_contents SET quantity = quantity + -1 , updated_at = :updated_at WHERE barnum = :barnum');
+            $stmt->bindValue(':barnum', $barnum);
+            $stmt->bindValue(':updated_at', $date, PDO::PARAM_STR);
+            $stmt->execute();
+    
+            // 売上票記入用
+            $stmt = $pdo->prepare('INSERT INTO earnings (barnum, created) VALUES(:barnum, :created)');
+            // $stmt->bindValue(':date', $created_at->format('Y-m-d H:i:s'), PDO::PARAM_STR);
+            $stmt->bindValue(':barnum', $barnum);
+            $stmt->bindValue(':created', $date, PDO::PARAM_STR);
+            $stmt->execute();
 
-        // 個数変更処理
-        $stmt = $pdo->prepare('UPDATE product_contents SET quantity = quantity + -1 , updated_at = :updated_at WHERE barnum = :barnum');
-        $stmt->bindValue(':barnum', $barnum);
-        $stmt->bindValue(':updated_at', $date, PDO::PARAM_STR);
-        $stmt->execute();
-
-        // 売上票記入用
-        $stmt = $pdo->prepare('INSERT INTO earnings (barnum, created) VALUES(:barnum, :created)');
-        // $stmt->bindValue(':date', $created_at->format('Y-m-d H:i:s'), PDO::PARAM_STR);
-        $stmt->bindValue(':barnum', $barnum);
-        $stmt->bindValue(':created', $date, PDO::PARAM_STR);
-        $stmt->execute();
-        $return["msg"] = 'Done itemname : '. $check_data[1]; 
-
+            $return["error"] = false;
+            $return["msg"] = 'Done itemname : '. $check_data[1]; 
+    
+        }else{
+            // 在庫なし
+            $return["error"] = true;
+            $return["msg"] = "在庫がありません。 : " . $check_data[1];
+        }
     }else{
-        // 在庫なし、存在しない //
+        // データベース存在なし
         $return["error"] = true;
-        $return["msg"] = "NoZaiko or Sonzainasi";
+        $return["msg"] = "データベース登録されていません。";
     }
+
 } catch (\Throwable $e) {
     $return["error"] = true;
     $return["msg"] = $e;
